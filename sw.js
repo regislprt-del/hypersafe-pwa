550d56b7b081b55236166308ec09d3a41994822b
regislprt-del <regislprt@gmail.com>
2026-08-29 13:03:37 +0200

    Ensure daily slot reset updates at midnight

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hypersafe-pwa-v5';
-const CORE=['./','./index.html','./styles.css','./config.js','./app-core.js','./app-ui.js','./auth-redirect.js','./push-notifications.js','./manual-adjust.js','./reset-control.js','./manifest.webmanifest','./logo.svg','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='hypersafe-pwa-v6';
+const CORE=['./','./index.html','./styles.css','./config.js','./app-core.js','./app-ui.js','./auth-redirect.js','./push-notifications.js','./manual-adjust.js','./reset-control.js','./day-reset.js','./manifest.webmanifest','./logo.svg','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
